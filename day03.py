24141e6c50b91ea63849e484168234bc65c9d628
kimseongmin <kpmgksm@naver.com>
2023-01-16 15:35:41 +0900

    day03_prime number mission version

--- day03.py
+++ day03.py
@@ -1,15 +1,21 @@
-# prime number 반복횟수 줄이기 + 연산과정 줄이기
+#mission in class
+#1. 숫자 2개 입력받음
+#2. 입력 받은 값 들 사이의 소수 구하는 프로그램 작성
 
-number = int(input('정수 입력.: '))
+num1, num2 = map(int, input("두개 정수 입력 : ").split())           #시작점 끝점 입력받음
 
-is_prime = True                 # count +=1에서의 2개의 연산과정 줄일 수 있음 + True/False타입이 더 작음
+if num1 > num2 :
+    num1, num2 = num2,num1                                     #파이썬만 가능한 문법
 
-for i in range(2, number):
-    if number % i == 0:
-        is_prime = False
-        break                   # 의미 없는 반복 줄이기 위함 -> 소수가 아닐 때 성능 up
-if is_prime:
-    print(f'{number} is prime Number.')
-else:
-    print(f'{number} is not prime Number.')
+for i in range (num1, num2+1):                                # 입력한 수 만큼 소수 찾기 반복하는 for문
+    #is_prime = True                                       # 소수 판단 변수 초기화(필요 없음 if문이 있기때문)
+    if i <= 1:
+        continue
+    for j in range(2,i):                              # 소수 판단 위한 for문
+        if i % j == 0:                                # if 1
+            #is_prime = False
+            break
+    else:                                            # if 1과 매칭되는 else문
+        print(i, end = '\t')
+
 
