290a7e5524a42252cafbceb4aad7e687af250ed7
kimseongmin <kpmgksm@naver.com>
2023-01-16 14:42:56 +0900

    day03_prime number end version

--- day03.py
+++ day03.py
@@ -1,14 +1,15 @@
-# prime number 반복횟수 줄이기
+# prime number 반복횟수 줄이기 + 연산과정 줄이기
 
 number = int(input('정수 입력.: '))
 
-count = 0
+is_prime = True                 # count +=1에서의 2개의 연산과정 줄일 수 있음 + True/False타입이 더 작음
 
 for i in range(2, number):
     if number % i == 0:
-        count +=1
-if count:                       # count가 0 이 아니면
-    print(f'{number} is not prime Number.')
-else:
+        is_prime = False
+        break                   # 의미 없는 반복 줄이기 위함 -> 소수가 아닐 때 성능 up
+if is_prime:
     print(f'{number} is prime Number.')
+else:
+    print(f'{number} is not prime Number.')
 
