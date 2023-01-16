fdd201278a5bbababeb904c8bbd76a2e63a2b3e1
kimseongmin <kpmgksm@naver.com>
2023-01-16 14:28:28 +0900

    day03_prime number by while

--- day03.py
+++ day03.py
@@ -1,11 +1,13 @@
-# prime number by 'for'
+# prime number by 'while'
 
 number = int(input('정수 입력.: '))
 
 count = 0
-for i in range(1, number+1):
-    if number % i == 0:
+j = 1
+while j <= number:
+    if number % j == 0:
         count +=1
+    j+=1
 if count == 2:
     print(f'{number} is prime Number.')
 else:
