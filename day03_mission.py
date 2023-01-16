5761e58f44931d8db262136cc1ebe0839902fa76
kimseongmin <kpmgksm@naver.com>
2023-01-16 16:27:29 +0900

    day03_5.6,5.7,5.8 finish

--- day03_mission.py
+++ day03_mission.py
@@ -1,8 +1,17 @@
-#Q 5.6
-ducky = 'ducky'
-Gourdy = 'Gourdy'
-Spitzy = 'Sitzy'
+#Q 5.6,5.7,5.8
+Ducky = 'Duck'
+Gourdy = 'Gourd'
+Spitzy = 'Sitz'
 
-print('%s Mc%sface' %(ducky,ducky))
-print('%s Mc%sface' %(Gourdy,Gourdy))
-print('%s Mc%sface' %(Spitzy,Spitzy))
+latter = '{0}y Mc{0}face'
+print('%sy Mc%sface' %(Ducky,Ducky))             #5.6
+print('%sy Mc%sface' %(Gourdy,Gourdy))
+print('%sy Mc%sface' %(Spitzy,Spitzy))
+print()
+print(latter.format(Ducky))                     #5.7
+print(latter.format(Gourdy))
+print(latter.format(Spitzy))
+print()
+print(f'{Ducky}y Mc{Ducky}face')                 #5.8
+print(f'{Gourdy}y Mc{Gourdy}face')
+print(f'{Spitzy}y Mc{Spitzy}face')
