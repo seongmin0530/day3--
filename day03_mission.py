b746e38068c32943aa2a91e760e3412c33227bbd
kimseongmin <kpmgksm@naver.com>
2023-01-16 16:31:23 +0900

    day03_6.2

--- day03_mission.py
+++ day03_mission.py
@@ -1,17 +1,13 @@
-#Q 5.6,5.7,5.8
-Ducky = 'Duck'
-Gourdy = 'Gourd'
-Spitzy = 'Sitz'
-
-latter = '{0}y Mc{0}face'
-print('%sy Mc%sface' %(Ducky,Ducky))             #5.6
-print('%sy Mc%sface' %(Gourdy,Gourdy))
-print('%sy Mc%sface' %(Spitzy,Spitzy))
-print()
-print(latter.format(Ducky))                     #5.7
-print(latter.format(Gourdy))
-print(latter.format(Spitzy))
-print()
-print(f'{Ducky}y Mc{Ducky}face')                 #5.8
-print(f'{Gourdy}y Mc{Gourdy}face')
-print(f'{Spitzy}y Mc{Spitzy}face')
+#Q 6.2
+guess_me = 7
+number = 1
+while True:
+    if number < guess_me:
+        print('Too low')
+    elif number > guess_me:
+        print('oops')
+        break
+    else:                           # guess_me == number
+        print('found it!')
+        break
+    number += 1
