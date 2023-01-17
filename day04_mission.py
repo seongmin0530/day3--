562f3ce8483f6fcb68616f9947d393ccae853210
kimseongmin <kpmgksm@naver.com>
2023-01-17 16:30:10 +0900

    day04_mission 7_11

--- day04_mission.py
+++ day04_mission.py
@@ -1,3 +1,25 @@
-# 7-10
-even = [number for number in range(10) if (number % 2 == 0 and number != 0)]
-print(even)
+# 7-11
+start1 = ["free", "file", "foe"]
+rhymes = [
+    ("flop", 'get a mop'),
+    ("fope", "turn the rope"),
+    ("fa", "get your ma"),
+    ("fudge", "call the judge"),
+    ("fat", "pet the cat"),
+    ("fog", "walk the dog"),
+    ("fun", "say we're done")
+    ]
+start2 = "Someone better"
+
+rhyme1 = rhymes[0][0].capitalize()  # first
+for i in start1:
+    print(i.capitalize(), end="! ")
+print(rhyme1, end="! \n")
+
+rhyme2 = list(rhymes[0])  #second
+del(rhyme2[0])
+print(start2, ' '.join(rhyme2), '.')
+
+
+
+
