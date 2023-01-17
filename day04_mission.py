9360e7d004bfa6fac146f0dbd4c2489a9418e4fc
kimseongmin <kpmgksm@naver.com>
2023-01-17 16:50:12 +0900

    day04_mission 8_1,2,3,4,5

--- day04_mission.py
+++ day04_mission.py
@@ -1,25 +1,19 @@
-# 7-11
-start1 = ["free", "file", "foe"]
-rhymes = [
-    ("flop", 'get a mop'),
-    ("fope", "turn the rope"),
-    ("fa", "get your ma"),
-    ("fudge", "call the judge"),
-    ("fat", "pet the cat"),
-    ("fog", "walk the dog"),
-    ("fun", "say we're done")
-    ]
-start2 = "Someone better"
+# 8-1, 8-2, 8-3, 8-4, 8-5
+e2f = dict(dog='chien', cat='chat', walrus='morse')  # 8-1
 
-rhyme1 = rhymes[0][0].capitalize()  # first
-for i in start1:
-    print(i.capitalize(), end="! ")
-print(rhyme1, end="! \n")
+print('#8-2: ',e2f['walrus'])  # 8-2
 
-rhyme2 = list(rhymes[0])  #second
-del(rhyme2[0])
-print(start2, ' '.join(rhyme2), '.')
+f2e = dict()  # 8-3
+for key, value in e2f.items():
+    f2e[value] = key
+print('#8-3: ',f2e)
 
+for key, value in e2f.items():  # 8-4
+    if value == 'chien':
+        print('#8-4: ',key)
 
+print('#8-5: ',end=' ')  # 8-5
+for key in e2f.keys():
+    print(key, end=' ')
 
 
