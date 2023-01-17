bb2bbad43a77a0de194eaf1a6bd4247d967496c8
kimseongmin <kpmgksm@naver.com>
2023-01-17 17:51:46 +0900

    day04_mission 8_14

--- day04_mission.py
+++ day04_mission.py
@@ -1,19 +1,5 @@
-# 7-11
-start1 = ["free", "file", "foe"]
-rhymes = [
-    ("flop", 'get a mop'),
-    ("fope", "turn the rope"),
-    ("fa", "get your ma"),
-    ("fudge", "call the judge"),
-    ("fat", "pet the cat"),
-    ("fog", "walk the dog"),
-    ("fun", "say we're done")
-    ]
-start2 = "Someone better"
-
-for i in rhymes:
-    for j in start1:
-        print(j.capitalize(), end="! ")
-    print(i[0].capitalize(), end="! \n")
-    rhyme2 = list(i)  #second
-    print(start2, rhyme2[1], '.\n \n')
+# 8-14
+titles = ['Creature of Habit', 'Crewel Fate']
+plots = ['A num turns in to a mon ster', 'A haunted yarn shop']
+movies = dict(zip(titles,plots))
+print(movies)
