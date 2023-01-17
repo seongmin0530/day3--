5d8caff451d3f60c73c8651e27aca853e8d83b35
kimseongmin <kpmgksm@naver.com>
2023-01-17 17:01:04 +0900

    day04_mission 8_10

--- day04_mission.py
+++ day04_mission.py
@@ -1,12 +1,5 @@
-# 8-6, 8-7, 8-8, 8-9
-life = {'animal':dict(cats='Henri', octopi='Grumpy', emus='Lucy'), 'plants': dict(), 'other': dict()}  #8-6
-
-first_key_list = [key for key in life.keys()]  # 8-7
-print('#8-7: ', first_key_list)
-
-animals_key_list = [key for key in life['animal'].keys()]  # 8-8
-print('#8-8: ', animals_key_list)
-
-print('#8-9: ',life['animal']['cats'])  # 8-9
+# 8-10
+squares = {key : key**2 for key in range(10)}
+print(squares)
 
 
