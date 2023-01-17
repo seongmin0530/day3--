3db1934fd0b7c667bbe7b264bd34ed52b2013ea8
kimseongmin <kpmgksm@naver.com>
2023-01-17 10:56:49 +0900

    day04_list

--- day04.py
+++ day04.py
@@ -1,22 +1,31 @@
-# tuple
-a = 'harry',  #packing
-b = ()  #empty tuple
-c = 'harry',  #packing
-d = ('harry',)  #packing
-e = 'harry','ron'  #packing
-f = ('harry','ron')  #packing
-g = ('hermione')  #string
-print(id(g))  #before +=
-g = g + f  #덮어쓰기
-print (g,id(g))  #after +=
-print(type(a))
-print(type(b))
-print(type(c))
-print(type(d))
-print(type(e))
-print(type(e))
-print(type(g))
+# list
 
-x, y = f
-print(x)  #unpacking
-print(y)  #unpacking
+# scores = ('B+', 'A+', 'C+')
+# print(scores[1],scores[2])
+# temp = list(scores)
+# temp[1] = 'c+'
+# temp[2] = 'A+'
+# scores = tuple(temp)
+# print(scores[1],scores[2])
+
+big_bang = ['GD','태양', 'TOP','대성', '승리']
+exo = ['백현', '첸']
+
+big_bang.append('인하')  #list 맨뒤에 요소 추가
+print('append. : ', big_bang)
+
+big_bang.insert(1,'KEB')  #list 중간에 요소 추가
+print('insert. :', big_bang)
+
+print('list의 복제 :', big_bang * 2)  #list의 복제
+
+exo.extend(big_bang)  #list의 결합 == exo = exo + big_bang
+print('extend. :',exo)
+
+#print(exo.pop())  #빅뱅 전체 pop
+print(exo[2].pop())  #빅뱅의 승리 pop
+print(exo.[2].pop(-2))  #빅뱅의 탑 pop
+exo.remove('인하')  #인하 삭제
+
+#+del
+
