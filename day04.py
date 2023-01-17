2f36f7deca2f600b63bc3c50261b184ccdc00b56
kimseongmin <kpmgksm@naver.com>
2023-01-17 16:09:42 +0900

    day04_mission 7_8,9

--- day04.py
+++ day04.py
@@ -1,53 +1,6 @@
-# Dictionary
+# Dictionary 02
 
-students = {'name': 'kin inha', 'age': 23, 'eyes': [0.9, 1.1]}
-
-# for i in students.key():  #bring only 'key'
-# for i in students.values():  # bring only 'value'
-# for i in students:  #bring 'key : value'
-
-# ************************************************************************************************
-# personal information
-# ************************************************************************************************
-# for k,v in students.items():  # bring 'key', 'value' by unpacking
-#     print(f'{k} : {v}')
-#
-# print(f'이름은 {students["name"]}, 나이는{students["age"]}세,', end=' ')
-# print(f'시력은 좌: {students["eyes"][0]}, 우: {students["eyes"][1]}')
-
-
-# ************************************************************************************************
-# alcohol suggestion
-# ************************************************************************************************
-import random
-
-alcohol_foods = {
-    '맥주' : '치킨',
-    '소주' : '골뱅이소면',
-    '와인' : '치즈',
-    '고량주' : '짬뽕'
-}
-
-alcohol_list = list(alcohol_foods)  # only key
-
-# food_list = [food for food in alcohol_foods.values()]  #only value
-
-while True:
-    alcohol = input(f'술을 선택하세요 0) 랜덤 1){alcohol_list[0]} 2){alcohol_list[1]} 3){alcohol_list[2]} 4){alcohol_list[3]} :')
-    print(f'{alcohol}을 선택하셨습니다.')
-    if alcohol == '5' :
-        print('다음에 또 오세요.')
-        break
-    elif alcohol == '0' :
-        print(f'{random.choice(alcohol_list)}에 어울리는 안주는 {alcohol_foods[random.choice(alcohol_list)]} 입니다.')
-#       print(f'{random.choice(alcohol_list)}에 어울리는 안주는 {random.choice(food_list)]} 입니다.')
-    elif alcohol == '1' :
-        print(f'{alcohol_list[0]}에 어울리는 안주는 {alcohol_foods[alcohol_list[0]]} 입니다.')
-    elif alcohol == '2' :
-        print(f'{alcohol_list[1]}에 어울리는 안주는 {alcohol_foods[alcohol_list[1]]} 입니다.')
-    elif alcohol == '3':
-        print(f'{alcohol_list[2]}에 어울리는 안주는 {alcohol_foods[alcohol_list[2]]} 입니다.')
-    elif alcohol == '4':
-        print(f'{alcohol_list[3]}에 어울리는 안주는 {alcohol_foods[alcohol_list[3]]} 입니다.')
-    else:
-        print('1~4사이의 메뉴를 입력해주세요.')
+first = {'a' : 'agony', 'b' : 'bliss'}
+second = {'b' : 'bagels', 'c' : 'candy'}
+third = {**first, **second}
+print(third)
--- day04_mission.py
+++ day04_mission.py
@@ -1,4 +1,5 @@
-# 7-1,7-2,7-3
-year_lists = [1999, 2000, 2001, 2002, 2003]  # 7-1
-print(year_lists[2])  # 7-2
-print(year_lists[-1])  # 7-3
+# 7-8, 7-9
+surprise = ['Groucho', 'Chio', 'Harpo']  # 7-8
+
+surprise[-1] = surprise[-1].lower()[::-1].capitalize()
+print(surprise[-1])  # 7-9
