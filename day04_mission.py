cf68a93a88014916030e8a7527fb55b43a6c53c6
kimseongmin <kpmgksm@naver.com>
2023-01-17 16:58:29 +0900

    day04_mission 8_6,7,8,9

--- day04_mission.py
+++ day04_mission.py
@@ -1,19 +1,12 @@
-# 8-1, 8-2, 8-3, 8-4, 8-5
-e2f = dict(dog='chien', cat='chat', walrus='morse')  # 8-1
+# 8-6, 8-7, 8-8, 8-9
+life = {'animal':dict(cats='Henri', octopi='Grumpy', emus='Lucy'), 'plants': dict(), 'other': dict()}  #8-6
 
-print('#8-2: ',e2f['walrus'])  # 8-2
+first_key_list = [key for key in life.keys()]  # 8-7
+print('#8-7: ', first_key_list)
 
-f2e = dict()  # 8-3
-for key, value in e2f.items():
-    f2e[value] = key
-print('#8-3: ',f2e)
+animals_key_list = [key for key in life['animal'].keys()]  # 8-8
+print('#8-8: ', animals_key_list)
 
-for key, value in e2f.items():  # 8-4
-    if value == 'chien':
-        print('#8-4: ',key)
-
-print('#8-5: ',end=' ')  # 8-5
-for key in e2f.keys():
-    print(key, end=' ')
+print('#8-9: ',life['animal']['cats'])  # 8-9
 
 
