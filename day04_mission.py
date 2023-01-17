d984535f002afdf68a1b25e65c708726a21958cb
kimseongmin <kpmgksm@naver.com>
2023-01-17 17:47:06 +0900

    day04_mission 7_11_end

--- day04_mission.py
+++ day04_mission.py
@@ -1,3 +1,19 @@
-# 8-12
-a = dict(zip(('optimist', 'pessimist', 'troll',),('The glass is half full', 'The glass is half empty', 'How did you get a glass')))
-print(a)
+# 7-11
+start1 = ["free", "file", "foe"]
+rhymes = [
+    ("flop", 'get a mop'),
+    ("fope", "turn the rope"),
+    ("fa", "get your ma"),
+    ("fudge", "call the judge"),
+    ("fat", "pet the cat"),
+    ("fog", "walk the dog"),
+    ("fun", "say we're done")
+    ]
+start2 = "Someone better"
+
+for i in rhymes:
+    for j in start1:
+        print(j.capitalize(), end="! ")
+    print(i[0].capitalize(), end="! \n")
+    rhyme2 = list(i)  #second
+    print(start2, rhyme2[1], '.\n \n')
