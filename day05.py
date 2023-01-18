95797c24157ed79f9367aae16bbd3c2f43fb2311
kimseongmin <kpmgksm@naver.com>
2023-01-18 13:34:50 +0900

    day05_function 02_calculate pay

--- day05.py
+++ day05.py
@@ -1,25 +1,53 @@
-# function prime number
-def is_prime(number):
-    """
-    매개변수로 받은 정수가 소수인지 여부를 판정하는 함수
-    :param number:
-    :return: True or False
-    """
+# function
 
-    if number <= 1:
-        return False
-    for k in range(2, number):
-        if number % k == 0:
+import random
+
+def make_person():
+    """
+    인원수, 나이 생성하는 함수
+    :return: 인원수, 각각의 나이 리스트
+    """
+    person_number = random.randint(0, 10)
+    ages = [random.randint(1, 50) for age in range(person_number)]
+    return person_number, ages
+
+
+def calculate_free(args):
+    """
+    놀이공원 요금 계산 프로그램
+    :param args: ages
+    :return: 지불할 총 입장료, 어른 수, 아이 수
+    """
+    total = 0
+    adults = 0
+    kids = 0
+    for age in args:
+        if 19 <= age:
+            total = total + 10000
+            adults = adults + 1
+        elif 1 <= age <= 19:
+            total = total + 3000
+            kids = kids + 1
+        else:
             return False
-    else:
-        return True
+    return total, kids, adults
 
-start = int(input("시작값을 입력하세요: "))
-end = int(input("시작값을 입력하세요: "))
 
-if start > end :
-    start, end = end, start
+person_number, ages = make_person()
+total, kids, adults = calculate_free(ages)
+print (f'환영합니다.\n총 {person_number}명이 입장하셨습니다.\n그 중 성인은 {adults}명, 학생은 {kids}명이며,\n총 입장료는 {total}원 입니다.')
 
-for i in range(start,end+1):
-    if is_prime(i):
-        print(i, end='\t')
+
+
+
+
+# def do_nothing():
+#     pass
+#
+#
+# do_nothing()
+# print(do_nothing())
+
+# mamamoo = ['화사', '솔라', '휘인', '문별']
+# print(mamamoo.remove('문별'))
+# print(mamamoo)
