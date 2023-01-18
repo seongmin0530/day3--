e44006ba1a12d585460ff6e5a2401a4e6d27ab6b
kimseongmin <kpmgksm@naver.com>
2023-01-18 15:27:55 +0900

    day05_function Closures

--- day05.py
+++ day05.py
@@ -1,35 +1,18 @@
-# function
+# function Closures
 
-def answer():
-    """
-    60 프린트하는 함수
-    :return:
-    """
-    print(60)
+def calculate():
+    x = 1
+    y = 2
+    temp = 0
+    def add_sub(n):
+        nonlocal temp
+        # x = 11  # local variable
+        temp = temp + x + n - y
+        return temp
+    return add_sub
 
-
-def call_func(func):
-    """
-    매개변수로 함수를 넘겨받아 실행시키는 함수
-    :param func:다른 함수
-    :return:
-    """
-    func()
-
-
-def subtract(num1, num2):
-    print(num1-num2)
-
-
-def run_func(func, arg1, arg2):
-    """
-    함수를 매개변수로 받아 함수 안에서 해당 함수를 실행
-    :param func: 함수
-    :param arg1: 정수 값
-    :param arg2: 정수 값
-    :return:
-    """
-    func(arg1, arg2)
-
-
-run_func(subtract, 99, 88)
+c1 = calculate()  # c1이 calculate()함수의 번지주소를 가지고 있어 언제든 사용할 수 있게 해줌
+for i in range(4):
+    print(c1(i),end='\t')  # 일반함수와는 다르게 함수가 종료되어도 지역변수가 남아있음....?
+print(type(c1))
+print(c1)
