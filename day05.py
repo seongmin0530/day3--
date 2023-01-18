b312bd900639ba34ab12fe3fb96c6934178ce3e4
kimseongmin <kpmgksm@naver.com>
2023-01-18 15:43:53 +0900

    day05_function lambda

--- day05.py
+++ day05.py
@@ -1,18 +1,36 @@
-# function Closures
+# function lamba
+# 람다 = 일회성 익명 함수 =>함수를 1회용할 때 사용
 
-def calculate():
-    x = 1
-    y = 2
-    temp = 0
-    def add_sub(n):
-        nonlocal temp
-        # x = 11  # local variable
-        temp = temp + x + n - y
-        return temp
-    return add_sub
+import random
+#
+#
+# def process(num_list, func):
+#     for num in num_list:
+#         print(func(num), end='\t')
+#
+#
+# def squares(n):
+#     """
+#     매개변수를 제곱하여 return하는 한ㅁ수
+#     :param n: intager
+#     :return: intager
+#     """
+#     return n**2
+#
+#
+# numbers = [random.randint(1, 100) for i in range(5)]
+# print(numbers)
+# process(numbers, squares)
 
-c1 = calculate()  # c1이 calculate()함수의 번지주소를 가지고 있어 언제든 사용할 수 있게 해줌
-for i in range(4):
-    print(c1(i),end='\t')  # 일반함수와는 다르게 함수가 종료되어도 지역변수가 남아있음....?
-print(type(c1))
-print(c1)
+
+# ******************************************************
+# Use lambda
+# ******************************************************
+def process(num_list, func):
+    for num in num_list:
+        print(func(num), end='\t')
+
+
+numbers = [random.randint(1, 100) for i in range(5)]
+print(numbers)
+process(numbers, lambda x: x * x)
