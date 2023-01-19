94e3b9383bf958d0aacebd2fe52db755ff128361
kimseongmin <kpmgksm@naver.com>
2023-01-19 15:47:22 +0900

    day06_class_inherit

--- day06.py
+++ day06.py
@@ -1,7 +1,10 @@
 # class
 
-class Pokemon:  #클래스
-    def __init__(self, name, owner, skils):  #객체 초기화 메서드
+class Pokemon:  # 클래스
+    # *************************************************************************#
+    # 상속되어 공유될 코드
+    # *************************************************************************#
+    def __init__(self, name, owner, skils):  # 객체 초기화 메서드
         self.name = name  # 속성
         self.owner = owner  # 속성
         self.skils = skils.split('/')
@@ -11,8 +14,15 @@ class Pokemon:  #클래스
         print(f"\n{self.owner}의 포켓몬은 {self.name}입니다.")
         for skil in self.skils:
             print(f"{self.name}이 사용할 수 있는 기술은 {skil}입니다.")
+    # ************************************************************************#
 
+class Pikachu(Pokemon):
+    pass
+
+
+pi1 = Pikachu('피카츄', '덴트', '백만볼트')
 p1 = Pokemon('피카츄', '한지우', '50만 볼트/100만 볼트/번개')  # p1 = 인스턴스
 p2 = Pokemon('꼬부기', '오바람', '고속스핀/하이드로펌프/몸통박치기/거품')  # p2 = 인스턴스
+pi1.info()
 p1.info()
 p2.info()
