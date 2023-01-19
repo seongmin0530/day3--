12b75996add0fd5516ffa44decb0a850c3edcbcd
kimseongmin <kpmgksm@naver.com>
2023-01-19 14:23:03 +0900

    day06_function_9-2

--- day06.py
+++ day06.py
@@ -1,29 +1,37 @@
-# recursion
+# exception
+try:
+    raise Exception("쉬는 시간")
+    expr = input('분자,분모 입력 : ').split()
+    print(int(expr[0]) / int(expr[1]))
 
-def factorial_iter(n):
-    """
-    반복문을 사용한 팩토리얼 함수
-    :param n: n!
-    :return: integer 팩토리얼 계산 결과 값
-    """
-    result = 1
-    for k in range(1,n+1):
-        result = result * k
-    return result
+except ValueError as err:
+    print('숫자를 입력해주세요.')
 
+except ZeroDivisionError as err:
+    print('분모에 0이 올 수 없습니다.')
+except IndexError as err
+    print('범위에 문제가 있습니다.')
+except Exception as other:
+    print('예외 발생')
+else:  #예:외가 발생하지 않앗을 때
+    print('프로그램 정상 ', end = ' ')
+finally():  #예외 발생 여부와 상관 없이 무조건 실행
 
-def factorial_recu(n):
-    """
-    재귀 함수를 사용한 팩토리얼 계산 함수
-    :param n: n!
-    :return: 자기 자신을 호출 또는 1
-    """
-    if n != 1:
-        return n * factorial_iter(n-1)
-    elif n == 1:
-        return 1
+print('종료')
 
 
-print(factorial_iter(5))  # 반복문을 이용한 factorial 함수 결과 출력
-print(factorial_recu(5))  # 재귀 함수를 이용한 factorial 함수 결과 출력
 
+
+
+
+# def div_calc(a, b):
+#     """
+#     나누기 함수
+#     :param a:분자
+#     :param b: 분모
+#     :return: 계산결과
+#     """
+#     return a / b
+#
+# print(div_calc(15, 3))
+# print(div_calc(15, 0))  # 에러 발생 (분자가 0이기 때문에 나눌 수 없음)
