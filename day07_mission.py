de452863254b8d0a1805c745c5513f8221bf3147
kimseongmin <kpmgksm@naver.com>
2023-01-20 16:30:57 +0900

    day07_mission_10.6

--- day07_mission.py
+++ day07_mission.py
@@ -1,11 +1,14 @@
-# 10.5
+# 10. 6
 class Element:
     def __init__(self, name, symbol, number):
         self.name = name
         self.symbol = symbol
         self.number = number
 
+    def dump(self):
+        print(f'Name: {self.name}\tSymbol: {self.symbol},\tNumber: {self.number}')
 
-el_dict = {'name': 'Hydrogen', 'symbol': 'H', 'number': 1}
+
+el_dict = {'name': 'hydrogen', 'symbol': 'H', 'number': 1}
 Hydrogen = Element(**el_dict)
-print(Hydrogen.name, Hydrogen.symbol, Hydrogen.number)
+Hydrogen.dump()
