d618ab56da9dcf14c6e4696a4fc36eb8dfae00d4
kimseongmin <kpmgksm@naver.com>
2023-01-20 15:39:10 +0900

    day07_class magic method

--- day07.py
+++ day07.py
@@ -1,4 +1,4 @@
-# class_duck typing
+# class_duck typing + 매직 매서드
 import math
 
 
@@ -38,13 +38,26 @@ class Rectangle(Shape):
     def get_area(self):
         return self.width * self.length
 
+    def __repr__(self):  #매직 매서드를 이용하여 출력 문구를 class안에 처리함
+        return f'사각형의 좌표는 x:{self.x}, y:{self.y}이고 넓이는 {self.get_area()}입니다'
+
+    def __add__(self, other):
+        # 두 사각형 넓이의 합
+        # return (self.width * self.length) + (other.width * other.length)
+
+        # 각 사각형 width의 합과 length의 합의 곱
+        return Rectangle(0, 0, self.width + other.width, self.length + other.length)
+
 
 cy1 = Cylinder(20, 20, 10.0, 2)
 c1 = Circle(100, 100, 10.0)
 c2 = Circle(50, 50, 2.0)
 r1 = Rectangle(100, 50, 5, 2)
+r2 = Rectangle(70, 30, 10, 15)
 
-print(cy1.get_area())
-print(f'사각형의 좌표는 x:{r1.x}, y:{r1.y}이고 넓이는 {r1.get_area()}입니다')
-print(c1.get_area())
-print(c2.get_area())
+# print(cy1.get_area())
+print(r1)
+print(r2)  # r1을 입력했을 때 매직 메서드에 의해 문구가 출력됨.
+print(r1+r2)
+# print(c1.get_area())
+# print(c2.get_area())
