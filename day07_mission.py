62ebef2391c8411c6c75ea3e9b46f721f9d3c6c5
kimseongmin <kpmgksm@naver.com>
2023-01-20 16:44:09 +0900

    day07_mission_10.9

--- day07_mission.py
+++ day07_mission.py
@@ -1,22 +1,23 @@
-# 10. 8
-class Element:
-    def __init__(self, name, symbol, number):
-        self.hidden_name = name
-        self.hidden_symbol = symbol
-        self.hidden_number = number
-
-    def get_value(self):
-        return self.hidden_name, self.hidden_symbol, self.hidden_number
-
-    def set_value(self, input_name, input_symbol, input_number):
-        self.hidden_name = input_name
-        self.hidden_symbol = input_symbol
-        self.hidden_number = input_number
-
-    def __str__(self):
-        return f'Name: {self.name}\tSymbol: {self.symbol},\tNumber: {self.number}'
+# 10. 9
+class Bear:
+    def eats(self):
+        return 'berries, (Bear)'
 
 
-el_dict = {'name': 'hydrogen', 'symbol': 'H', 'number': 1}
-Hydrogen = Element(**el_dict)
-print(Hydrogen.get_value())
+class Rabbit:
+    def eats(self):
+        return 'clover, (Rabbit)'
+
+
+class Octothorpe:
+
+    def eats(self):
+        return 'campers, (Octothorpe)'
+
+
+bear = Bear()
+rabbit = Rabbit()
+octothorpe = Octothorpe()
+print(bear.eats())
+print(rabbit.eats())
+print(octothorpe.eats())
